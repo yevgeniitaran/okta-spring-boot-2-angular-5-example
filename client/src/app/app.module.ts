219a6b2b3583ad168651bd98fe89a7dbd72cabaf
Yevgen Taran <yevgenii.taran@gmail.com>
2018-05-29 01:04:08 +0300

    angular-component

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -2,9 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { CarService } from './shared/car/car.service';
 import { HttpClientModule } from '@angular/common/http';
+import { MatButtonModule, MatCardModule, MatInputModule, MatListModule, MatToolbarModule } from '@angular/material';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
 import { CarListComponent } from './car-list/car-list.component';
+import { GiphyService } from './shared/giphy/giphy.service';
 
 @NgModule({
   declarations: [
@@ -13,9 +16,15 @@ import { CarListComponent } from './car-list/car-list.component';
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatCardModule,
+    MatInputModule,
+    MatListModule,
+    MatToolbarModule
   ],
-  providers: [CarService],
+  providers: [CarService, GiphyService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
